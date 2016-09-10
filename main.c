d82c82ee044af1aa77f512548806a9ce9dd9f4d0
Hendy Rodrigues Ferreira da Silva <mr.syslogg@gmail.com>
2016-09-10 11:46:34 -0300

    Carregamento de arquivo

--- main.c
+++ main.c
@@ -8,10 +8,12 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+char filename[]  = "db/BDAlunos10e1v1.txt"; //File test
+
 int main(int argc, char *argv[]) {
 	//Criação da arvore
 	Arvore * a = criar();
-	CarregarArquivo(a);
+	if(CarregarArquivo(a) == false) return 0;
 	
 	inserir(a,10);
 	inserir(a,8);
@@ -20,10 +22,33 @@ int main(int argc, char *argv[]) {
 	inserir(a,12);
 	
     imprimir(a);
-
+	
 	return 0;
 }
 
-void CarregarArquivo (Arvore * arv) {
+bool CarregarArquivo (Arvore * arv) {
 	//Carregar dados do Arquivo para arvore;
+	
+	FILE * file = fopen(filename, "r");
+	
+	if(file == NULL) {
+		clean();
+		printf("Erro: algum problema foi encontrado na leitura do arquivo.");
+		return false;
+	}
+	int matricula;
+	char nome[30], email[30], telefone[30];
+	//Percorre todo o arquivo
+	while ((fscanf(file,"%d | %s | %s | %s\n", &matricula, nome, &email, &telefone))!= EOF) {
+		printf("Nome: ",nome);
+	}
+	
+	
+	
+	return true;
+}
+
+
+void clean() {
+	system("cls");
 }
